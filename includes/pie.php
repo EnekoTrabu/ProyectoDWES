eabac275ec90ae1c12f2be5586c294bce087becd
itziar <itziar21roldan@gmail.com>
2019-12-05 09:07:47 +0100

    Añado icono y include pie

---
+++ includes/pie.php
@@ -0,0 +1,17 @@
+</section>
+    </main>
+    <footer>
+        <p>Adopcion canina y felina</p>
+        <p class="copyright">
+            &copy;
+            <?php
+            setlocale(LC_TIME, 'Spanish');
+            $fecha = strftime("%A, %d  %B %Y");
+            echo utf8_encode($fecha);
+            ?>
+        </p>
+        <p>Realizado por: Iranzu Aguirre, Eneko Trabudua e Itziar Roldan.</p>
+    </footer>
+</body>
+
+</html>
