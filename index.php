8750a802de2cf474f28d654e5381989bdd4acbb5
iranzu <iranzuaguirre@hotmail.com>
2019-12-09 10:49:08 +0100

    Añado formulario de añadir animal

--- index.php
+++ index.php
@@ -5,6 +5,115 @@ include 'includes/cabecera.php';
     <fieldset>
 
         <!-- Form Name -->
+        <legend>Añadir Nuevo Animal</legend>
+
+        <!-- Text input-->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="nombre">Nombre</label>
+            <div class="col-md-4">
+                <input id="nombre" name="nombre" type="text" placeholder="Nombre" class="form-control input-md">
+                <span class="help-block">Introduzca el nombre del animal</span>
+            </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="edad">Edad</label>
+            <div class="col-md-4">
+                <input id="edad" name="edad" type="text" placeholder="Edad" class="form-control input-md">
+                <span class="help-block">Introduzca la edad del animal</span>
+            </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="procedencia">Procedencia</label>
+            <div class="col-md-4">
+                <input id="procedencia" name="procedencia" type="text" placeholder="Procedencia" class="form-control input-md">
+                <span class="help-block">Introduzca la procedencia del animal</span>
+            </div>
+        </div>
+
+        <!-- Multiple Radios -->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="genero">Género</label>
+            <div class="col-md-4">
+                <div class="radio">
+                    <label for="genero-0">
+                        <input type="radio" name="genero" id="genero-0" value="Macho" checked="checked">
+                        Macho
+                    </label>
+                </div>
+                <div class="radio">
+                    <label for="genero-1">
+                        <input type="radio" name="genero" id="genero-1" value="Hembra">
+                        Hembra
+                    </label>
+                </div>
+            </div>
+        </div>
+
+        <!-- Select Basic -->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="tipo">Tipo de animal</label>
+            <div class="col-md-4">
+                <select id="tipo" name="tipo" class="form-control">
+                    <option value="Perro">Perro</option>
+                    <option value="Gato">Gato</option>
+                </select>
+            </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="raza">Raza</label>
+            <div class="col-md-4">
+                <input id="raza" name="raza" type="text" placeholder="Raza" class="form-control input-md">
+                <span class="help-block">Introduzca la raza del animal</span>
+            </div>
+        </div>
+
+        <!-- File Button -->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="foto">Foto</label>
+            <div class="col-md-4">
+                <input id="foto" name="foto" class="input-file" type="file">
+            </div>
+        </div>
+
+        <!-- Select Basic -->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="salud">Salud</label>
+            <div class="col-md-4">
+                <select id="salud" name="salud" class="form-control">
+                    <option value="buena">Buena salud</option>
+                    <option value="mala">Mala salud</option>
+                </select>
+            </div>
+        </div>
+
+        <!-- Textarea -->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="descripcion">Descripción</label>
+            <div class="col-md-4">
+                <textarea class="form-control" id="descripcion" name="descripcion">Introduzca una pequeña descripción del animal</textarea>
+            </div>
+        </div>
+
+        <!-- Button -->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="enviar"></label>
+            <div class="col-md-4">
+                <button id="enviar" name="enviar" class="btn btn-primary">Enviar</button>
+            </div>
+        </div>
+
+    </fieldset>
+</form>
+<form class="form-horizontal">
+    <fieldset>
+
+        <!-- Form Name -->
         <legend>Concertar Visita</legend>
 
         <!-- Text input-->
