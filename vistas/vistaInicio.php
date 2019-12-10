88732ac6bd02d9adc10d2f5e0048e564b2af715b
Eneko Trabudua <43170217+EnekoTrabu@users.noreply.github.com>
2019-12-10 17:48:17 +0100

    Pruebas con el valor oculto

--- vistas/vistaInicio.php
+++ vistas/vistaInicio.php
@@ -2,11 +2,20 @@
 <div class="row">
     <?php
         
-        if(isset($_SESSION['valorTarjeta'])){
+        if(isset($_GET['oculto'])){
+         $oculto = $_GET['oculto'];
+            if($oculto){
+                echo $oculto;
+                $_GET['oculto'] = false;
+            }else{
+                echo $oculto;   
+            }
+        }
+        /*if(isset($_SESSION['valorTarjeta'])){
             foreach($_SESSION['valorTarjeta'] as $tarjeta){
                 echo crearTarjeta($tarjeta['nombre'], $tarjeta['edad']);
             }
-        }
+        }*/
 
     ?>
 </div>
