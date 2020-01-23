ab84272c74e467dbadc4d9b0ad196e57d2e72ee2
itziar <itziar21roldan@gmail.com>
2020-01-23 09:25:07 +0100

    Empezar clase DataBase

--- modelo/DataBase.php
+++ modelo/DataBase.php
@@ -10,11 +10,10 @@ class DataBase implements IDataBase
 
         try {
 
-            $datos= "mysql:host=localhost;dbname=ejemplo_pdo";
+            $datos = "mysql:host=localhost;dbname=ejemplo_pdo";
             $this->conexion = new PDO($datos, 'alumno', 'alumno');
 
             return $this->conexion;
-
         } catch (PDOException $error) {
             echo "<p>Error: " . $error->getMessage() . "</p>";
             exit();
@@ -28,9 +27,26 @@ class DataBase implements IDataBase
 
     public function ejecutarSQL($sql)
     {
+        //los select
+        //Query
+        $db = $this->conectar();
+
+        if ($db->query($sql)) {
+            echo "  <p>Registro creado correctamente.</p>\n";
+        } else {
+            echo "  <p>Error al crear el registro.<p>\n";
+        }
+
+        $this->desconectar();
     }
 
     public function ejecutarSQLactualizacion($sql, $args)
     {
+        //insert, delete, update
+        //prepare
+        $db = $this->conectar();
+
+        $result = $db->prepare($sql);
+        $result->execute();
     }
 }
