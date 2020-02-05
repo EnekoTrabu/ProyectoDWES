8ce15baa4a51d5ea23e550af0aeaa8de90418581
Eneko <etrabudua5@gmail.com>
2020-02-05 09:35:36 +0100

    Cambios menores

--- includes/cabecera.php
+++ includes/cabecera.php
@@ -32,11 +32,6 @@
                 <li class="nav-item active">
                     <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
                 </li>
-            </ul>
-        </div>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
                     <a class="nav-link" href="index.php?animales=true">Ver Animales <span class="sr-only"></span></a>
                 </li>
