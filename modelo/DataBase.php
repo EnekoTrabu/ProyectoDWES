5dcffd3e2d6a658df0bf130e09bda5e8f340b581
Iranzu <iranzuaguirre@hotmail.com>
2020-02-17 10:55:18 +0100

    Quito comentarios

--- modelo/DataBase.php
+++ modelo/DataBase.php
@@ -65,17 +65,6 @@ class DataBase implements IDataBase
      */
     public function ejecutarSQL($sql)
     {
-        /*//los select
-        //Query
-        $db = $this->conectar();
-
-        if ($db->query($sql)) {
-            echo "  <p>Registro creado correctamente.</p>\n";
-        } else {
-            echo "  <p>Error al crear el registro.<p>\n";
-        }
-
-        $this->desconectar();*/
         try {
             $result = $this->conexion->query($sql);
             return $result;
@@ -102,12 +91,6 @@ class DataBase implements IDataBase
      */
     public function ejecutarSQLactualizacion($sql, $args)
     {
-        /*//insert, delete, update
-        //prepare
-        $db = $this->conectar();
-
-        $result = $db->prepare($sql);
-        $result->execute();*/
         try {
             $result = $this->conexion->prepare($sql);
             $result->execute($args);
