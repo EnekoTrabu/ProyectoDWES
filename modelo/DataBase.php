28192e2318dcf05f74ea6dbc053dd00da1b8b82a
Iranzu <iranzuaguirre@hotmail.com>
2020-02-20 08:41:31 +0100

    Eliminar base datos duplicada

--- modelo/DataBase.php
+++ modelo/DataBase.php
@@ -21,12 +21,12 @@ class DataBase implements IDataBase
 
         try {
 
-            //$datos = "mysql:host=localhost;dbname=adoptamedb";
-            //$this->conexion = new PDO($datos, 'root', '');
+            $datos = "mysql:host=localhost;dbname=adoptamedb";
+            $this->conexion = new PDO($datos, 'root', '');
 
             
-            $datos = "mysql:host=localhost;dbname=id12629186_adoptame";
-            $this->conexion = new PDO($datos, 'id12629186_alumno', 'alumno');
+            //$datos = "mysql:host=localhost;dbname=id12629186_adoptame";
+            //$this->conexion = new PDO($datos, 'id12629186_alumno', 'alumno');
             
             $this->conexion->exec("set names utf8");
 
